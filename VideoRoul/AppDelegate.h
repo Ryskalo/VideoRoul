3742ee322dde674365826308720c166cfd9ef422
[sibit11@gmail.com] <[sibit11@gmail.com]>
2016-01-28 21:56:48 +0300

    Initial Commit

---
+++ VideoRoul/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoRoul
+//
+//  Created by Антон Рыскалев on 28.01.16.
+//  Copyright © 2016 Антон Рыскалев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoRoul/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoRoul
+//
+//  Created by Антон Рыскалев on 28.01.16.
+//  Copyright © 2016 Антон Рыскалев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
